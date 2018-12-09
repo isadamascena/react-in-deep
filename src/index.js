92397eb0da7659ba745c24b56c6b9b1c8eecf2b2
isadamascena <idamasce@thoughtworks.com>
2018-12-09 19:48:53 -0300

    adding new component files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+ReactDOM.render( 
+    <App />,
+    document.querySelector('#root')
+)
