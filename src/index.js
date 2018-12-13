05755af7fc591320da539ce5eebd62b1d33c3054
isadamascena <idamasce@thoughtworks.com>
2018-12-12 22:44:48 -0300

    creating routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+//Components
+
+import Home from './components/home'
+import Profile from './components/profiles'
+import Posts from './components/posts'
 
 const App = () => {
     return (
-        <div>
-            Home
-        </div>
+        <BrowserRouter>
+            <div>
+                <header>
+                    Header
+                </header>
+                <Route path="/" exact component={Home} />
+                <Route path="/posts" component={Posts} />
+                <Route path="/profiles" component={Profile} />
+            </div>
+        </BrowserRouter>
     )
 }
 
