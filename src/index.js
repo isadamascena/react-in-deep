e3226a693a748983ec38f2c954d428edfcf1577c
isadamascena <idamasce@thoughtworks.com>
2018-12-15 12:15:00 -0300

    params and url's

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 
 //Components
 
 import Home from './components/home'
 import Profile from './components/profiles'
 import Posts from './components/posts'
+import PostItem from './components/post_item'
 
 const App = () => {
     return (
         <BrowserRouter>
             <div>
                 <header>
-                    <Link to="/">Home</Link><br/>
-                    <Link to="/posts">Posts</Link><br/>
-                    <Link to={{
+                    <NavLink to="/">Home</NavLink><br/>
+                    <NavLink 
+                        to="/posts"
+                        activeStyle={{color:'red'}}>
+                        Posts
+                    </NavLink><br/>
+                    <NavLink to={{
                         pathname: '/profiles',
-                    }}>Profile</Link><br/>
+                    }}>Profile</NavLink><br/>
                     <hr/>
                 </header>
                 <Route path="/" exact component={Home} />
-                <Route path="/posts" component={Posts} />
+                <Route path="/posts" exact component={Posts} />
+                <Route path="/posts/:id" component={PostItem} />
                 <Route path="/profiles" component={Profile} />
             </div>
         </BrowserRouter>
