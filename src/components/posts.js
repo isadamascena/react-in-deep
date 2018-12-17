99a904f9ba7928d74a725fbcfa669868f4f38a7b
isadamascena <idamasce@thoughtworks.com>
2018-12-16 22:45:06 -0300

    returning arrays

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Posts = () => {
-    return(
-        <div>
-            <Link to="/posts/1">post 1</Link><br/>
-            <Link to="/posts/2">post 2</Link><br/>
-            <Link to="/posts/3">post 3</Link><br/>
-        </div>
-    );
+
+    const ids = [
+        {id: 1, name: 'Post 1'},
+        {id: 2, name: 'Post 2'},
+        {id: 3, name: 'Post 3'}
+    ]
+
+    const endpoint = (number) => `/posts/${number}`
+
+    return ids.map(item => {
+        return (
+            <span key={item.id}>
+                <Link to={endpoint(item.id)}>{item.name}</Link><br/>
+            </span>
+        )
+    });
 }
 
 export default Posts;
