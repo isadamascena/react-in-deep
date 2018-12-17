f169cb5b0a24fbf5b74ace860db5e67509c60913
isadamascena <idamasce@thoughtworks.com>
2018-12-16 21:22:39 -0300

    react life cycles (continued)

--- src/components/life.js
+++ src/components/life.js
@@ -11,22 +11,48 @@ class Life extends Component {
 
     //3 before render 
     componentWillMount(){
-        console.log('3 before render')
+        console.log('3 before render');
     }
     //4 render jsx
     render(){
+        console.log('RENDER');
         return(
             <div>
                 <h3>{this.state.title}</h3>
+                <div onClick={()=> this.setState({title: 'something new'})}>
+                CHANGE TITLE
+                </div>
             </div>
         );
     }
 
     //5 after render
     componentDidMount(){
-        console.log('5 after render')
+        console.log('5 after render');
         document.querySelector('h3').style.color = 'red';
     }
 
+    componentWillUpdate() {
+        console.log('before update');
+    }
+
+    componentDidUpdate() {
+        console.log('after update');
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        if (nextState.title !== this.state.title){
+            return true;
+        }
+        return false;
+    }
+
+    componentWillReceiveProps(){
+        console.log('before receive props');
+    }
+
+    componentWillUnmount(){
+        console.log('UNMOUNT');
+    }
 }
 export default Life;
