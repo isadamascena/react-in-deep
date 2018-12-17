b82e356f79ea45588c852828cb610b559241a0ea
isadamascena <idamasce@thoughtworks.com>
2018-12-16 22:11:19 -0300

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Profile from './components/profiles'
 import Posts from './components/posts'
 import Life from './components/life'
 import PostItem from './components/post_item'
+import Conditional from './components/conditional'
 
 const App = () => {
     return (
@@ -25,12 +26,14 @@ const App = () => {
                         pathname: '/profiles',
                     }}>Profile</NavLink><br/>
                     <NavLink to="/life">Life</NavLink><br/>
+                    <NavLink to="/conditional">Conditional</NavLink><br/>
                     <hr/>
                 </header>
                 <Switch>
                     <Route path="/posts/:id" component={PostItem} />
                     <Route path="/posts" component={Posts} />
                     <Route path="/life" component={Life} />
+                    <Route path="/conditional" component={Conditional} />
                     <Route path="/profiles" component={Profile} />
                     <Route path="/" exact component={Home} />
                     <Route render={()=> <h3>oops 404</h3>} />
